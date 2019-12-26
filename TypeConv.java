7bb42a2be7020abc84b37c2feda6eff83ef7a815
kishan <kishansrivastava20@gmail.com>
2019-12-26 23:53:15 +0530

    Third Commit

---
+++ TypeConv.java
@@ -0,0 +1,10 @@
+package World;
+public class TypeConv
+{
+	public static void main(String[] args)
+	{
+		int a=7;
+		float x=a;        //this is a widing conversion
+		System.out.println("value of x is:"+ x);
+	}
+}
